1cbc5184cc9547c5e3b084fdd6a2ad15331115fb
Nikhil <anandvineet274@gmail.com>
2014-10-26 22:31:24 +0530

    commit#5

--- Pay2Coin/gen/com/example/pay2coin/R.java
+++ Pay2Coin/gen/com/example/pay2coin/R.java
@@ -1080,6 +1080,9 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int Add_To_Favorites=0x7f090054;
+        public static final int Pay_Again_Button=0x7f090053;
+        public static final int Pay_Now_Button=0x7f09004d;
         public static final int action_bar=0x7f09001c;
         public static final int action_bar_activity_content=0x7f090001;
         public static final int action_bar_container=0x7f09001b;
@@ -1091,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f090047;
+        public static final int action_settings=0x7f090055;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
@@ -1105,6 +1108,8 @@ containing a value of this type.
         public static final int editText1=0x7f090040;
         public static final int editText2=0x7f09003f;
         public static final int edit_query=0x7f090030;
+        public static final int edit_text_amount=0x7f09004c;
+        public static final int edit_text_billno=0x7f090049;
         public static final int end=0x7f090017;
         public static final int expand_activities_button=0x7f090026;
         public static final int expanded_menu=0x7f09002c;
@@ -1113,11 +1118,13 @@ containing a value of this type.
         public static final int icon=0x7f09002a;
         public static final int ifRoom=0x7f090011;
         public static final int image=0x7f090027;
+        public static final int linearLayout1=0x7f090047;
+        public static final int linearLayout2=0x7f09004a;
         public static final int listMode=0x7f090006;
         public static final int list_item=0x7f090029;
-        public static final int list_view_main=0x7f090045;
+        public static final int list_view_main=0x7f090046;
         public static final int middle=0x7f090018;
-        public static final int name=0x7f090046;
+        public static final int name=0x7f09004e;
         public static final int never=0x7f090012;
         public static final int none=0x7f090019;
         public static final int normal=0x7f090007;
@@ -1147,6 +1154,13 @@ containing a value of this type.
         public static final int textView1=0x7f090042;
         public static final int textView2=0x7f090041;
         public static final int textView3=0x7f090043;
+        public static final int text_view_amount=0x7f09004b;
+        public static final int text_view_billno=0x7f090048;
+        public static final int text_view_cost=0x7f090052;
+        public static final int text_view_date=0x7f09004f;
+        public static final int text_view_item=0x7f090051;
+        public static final int text_view_main=0x7f090045;
+        public static final int text_view_merchant=0x7f090050;
         public static final int title=0x7f09002b;
         public static final int top_action_bar=0x7f090020;
         public static final int up=0x7f090021;
@@ -1206,8 +1220,10 @@ containing a value of this type.
         public static final int activity_launcher=0x7f030018;
         public static final int activity_main=0x7f030019;
         public static final int fragment_main=0x7f03001a;
-        public static final int row=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int payment_launcher=0x7f03001b;
+        public static final int row=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int transaction_launcher=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1259,10 +1275,12 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
-        public static final int exit=0x7f0a0016;
+        public static final int exit=0x7f0a0018;
         public static final int hello_world=0x7f0a000e;
         public static final int logout=0x7f0a0014;
         public static final int title_activity_launcher=0x7f0a0015;
+        public static final int title_activity_payment=0x7f0a0016;
+        public static final int title_activity_transactions=0x7f0a0017;
     }
     public static final class style {
         /** 
